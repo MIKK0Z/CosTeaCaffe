601b8ced74a394e30927bf9d43151cddab28e175
mikey <michal_maslanka@onet.pl>
2025-02-10 20:07:50 +0100

    a

--- app/src/main/java/com/example/costeacaffe/SnackCategoryActivty.kt
+++ app/src/main/java/com/example/costeacaffe/SnackCategoryActivty.kt
@@ -1,13 +1,11 @@
 package com.example.costeacaffe
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.costeacaffe.SnackType
-import com.google.gson.Gson
+import java.io.BufferedReader
 import java.io.InputStreamReader
 
 class SnackCategoryActivty : AppCompatActivity() {
@@ -16,13 +14,30 @@ class SnackCategoryActivty : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_snack_category_activty)
 
-        val fileStream = this.assets.open("snacks.json")
-        val reader = InputStreamReader(fileStream)
-        val snacks = Gson().fromJson(reader, Array<SnackType>::class.java)
+//        val fileStream = this.assets.open("snacks.json")
+//        val reader = InputStreamReader(fileStream)
+//        val snacks = Gson().fromJson(reader, Array<SnackType>::class.java)
+        val snacksString = readFile(baseContext, "snacks.json")
 
 
 
 
 
+    }
+}
+
+fun readFile(context: Context, path: String): String {
+    try {
+        val file = context.assets.open(path);
+        val bufferedReader = BufferedReader(InputStreamReader(file));
+        val stringBuilder = StringBuilder();
+        bufferedReader.useLines { lines ->
+            lines.forEach { stringBuilder.append(it) };
+        };
+
+        return stringBuilder.toString();
+    } catch (err: Exception) {
+        Log.e("file reader", "error when reading file $path: $err");
+        return "";
     }
 }
